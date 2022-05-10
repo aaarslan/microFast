274dee5adf5cfb352cb786c3e08767a0fb9b1897
Abdallah Arslan <aarslan@Abdallahs-MacBook-Pro.local>
2022-05-10 14:23:20 -0400

    Initial Commit

---
+++ payment/main.py
@@ -0,0 +1,69 @@
+import time
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.background import BackgroundTasks
+from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+import requests
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+# This should be a different database but no money no honey
+redis = get_redis_connection(
+    host="redis-11579.c276.us-east-1-2.ec2.cloud.redislabs.com",
+    port="11579",
+    password="9J4osLi9dVS6J5xmJ2DzJCURcsWHUsX3",
+    decode_responses=True
+)
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str # pending, completed, refunded
+
+    class Meta:
+        database = redis
+
+@app.get('/orders/{pk}')
+def get(pk: str):
+    order = Order.get(pk)
+    redis.xadd('refund_order', order.dict(), '*')
+    return order
+
+
+@app.post('/orders')
+async def create(request: Request, background_tasks: BackgroundTasks): # id, quantity
+    body = await request.json()
+
+    req = requests.get('http://localhost:8000/products/%s' %body['id'])
+    product = req.json()
+
+    order = Order(
+        product_id=body['id'],
+        price=product['price'],
+        fee=0.2 * product['price'],
+        total=1.2 * product['price'],
+        quantity=body['quantity'],
+        status='pending'
+    )
+    order.save()
+
+    background_tasks.add_task(order_completed(order))
+
+    return order
+
+def order_completed(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
+    redis.xadd('order_completed', order.dict(), '*')
